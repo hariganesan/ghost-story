ef565c58c0a322f7fe5efbda1d589b14e33f9222
hariganesan <hari.ganesan@yale.edu>
2013-05-05 16:36:25 -0400

    m

--- src/Events.cpp
+++ src/Events.cpp
@@ -8,13 +8,14 @@ int MyGlWindow::handle(int event) {
 		case FL_FOCUS:
 		case FL_UNFOCUS:
 			return 1;
-			
+
 		case FL_KEYBOARD:
 			if (Fl::event_key() == 'g') {
-				s->mcG = s->mc->createGhost(s->mcP->getX(), s->mcP->getY());
-				s->mcP->possessed = false;
-				s->mcP = NULL;
-				redraw();
+				//s->mcG = s->mc->createGhost(s->mcP->getX(), s->mcP->getY());
+				//s->mcP->possessed = false;
+				//s->mcP = NULL;
+				//redraw();
+				std::cout << "pressed g" << std::endl;
 				return 1;
 			} else {
 				return 0;
