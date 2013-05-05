4c8998886e762748bccd73af8539c5fbd42a9bcd
hariganesan <hari.ganesan@yale.edu>
2013-05-05 15:27:09 -0400

    m

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,14 @@ int main(int argc, char **argv) {
 
 
 	// set up game
-	//Stage *s = new Stage(GRAVITY_DEF);
+	Stage *s = new Stage(GRAVITY_DEF);
 	//s->mc = new Character(s, "MC", 50, POS_X_START, POS_Y_START, CHAR_W_DEF, CHAR_H_DEF);
 	//s->mcP = NULL;
 	//s->enemies[0] = new Character(s, "Enemy1", 20, 400, POS_Y_START, CHAR_W_DEF, CHAR_H_DEF);
 	//s->enemies[0]->isPossessible = true;
 
 	Fl_Window win(WINDOW_WIDTH, WINDOW_HEIGHT, "Ghost Story");
-	MyGlWindow mygl(10, 10, win.w()-20, win.h()-20);
+	MyGlWindow mygl(s, 10, 10, win.w()-20, win.h()-20);
 	win.end();
 	win.resizable(mygl);
 	win.show();
